fe0315dfd779794f329fc32374bb70c9cd3e1929
Briance Miranda <mirandabriance@gmail.com>
2017-11-21 22:09:37 -0600

    Timeline API Fail

--- v01/js/orion.js
+++ v01/js/orion.js
@@ -30,23 +30,57 @@ $(document).ready(function(){
     } catch (err) {
         console.log("Ocurrio un error: " + err);
     }// fin catch
-    
+
+    try {
+        $.ajax({
+            async: true,
+            crossDomain: true,
+            url: "http://softwarerepositoryws.gonzalez.cr/api/SoftwareVisualization/GetAllPackageRevision?projectlongId=0002",
+            type: 'GET',
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: false
+            },
+            processData: false,
+
+            success: function (data, status) {
+                fillTimelineItems(data);
+            },
+
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("Estado de la conexión: " + textStatus + " ");
+                console.log("Error de conexión: " + errorThrown + " ");
+            }
+        });
+    } catch (err) {
+        console.log("Ocurrio un error: " + err);
+    }// fin catch
+});
+
+function fillTimelineItems(itemData){
     // DOM element where the Timeline will be attached
      var container = document.getElementById('visualization');
 
      // Create a DataSet (allows two way data-binding)
-     var items = new vis.DataSet([
-       {id: 1, content: 'item 1', start: '2017-04-20'},
-       {id: 2, content: 'item 2', start: '2017-04-14'},
-       {id: 3, content: 'item 3', start: '2017-04-18'},
-       {id: 4, content: 'item 4', start: '2017-04-16'},
-       {id: 5, content: 'item 5', start: '2017-04-25'},
-       {id: 6, content: 'item 6', start: '2018-04-27'}
-     ]);
+     var items = new vis.DataSet({
+        type: {start: 'ISODate', end: 'ISODate' }
+      });
+
+     var dates =[];
+
+     for(var i=0; i<200; i++){
+        var apiYear = JSON.stringify(itemData.resultado[i].year);
+        var apiMonth = JSON.stringify(itemData.resultado[i].month);
+        var apiDay = JSON.stringify(itemData.resultado[i].day);
+        dates.push({id: i, content: "item " +i, start: apiYear+"-"+apiMonth +"-"+apiDay});
+     }
+
+    items.clear();
+    items.add(dates);
 
      // Configuration for the Timeline
      var options = {};
 
      // Create a Timeline
      var timeline = new vis.Timeline(container, items, options);
-});
+}
